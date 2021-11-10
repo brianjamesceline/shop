f40b2f7366d63edd4a446ffece3c118a586803c6
Brian Celine <brianceline@gmail.com>
2021-11-10 01:00:00 +0000

    complete initial work on shop

---
+++ src/Components/FilterableProductTable.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SearchBar from './SearchBar'
+import ProductTable from './ProductTable'
+
+export default function FilterableProductTable(props) {
+  const { products } = props;
+
+  return (
+    <div>
+      <SearchBar />
+      <ProductTable products={products} />
+    </div>
+  )
+}
---
+++ src/Components/ProductTable.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import ProductCategoryRow from './ProductCategoryRow'
+import ProductRow from './ProductRow'
+
+export default function ProductTable(props) {
+  const { products } = props;
+
+  const rows = [];
+  let lastCategory = null;
+
+  products.forEach((product) => {
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow
+          category={product.category}
+          key={product.category}
+        />
+      )
+    }
+    rows.push(
+      <ProductRow
+        product={product}
+        key={product.name}
+      />
+    )
+    lastCategory = product.category;
+  })
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows}
+      </tbody>
+    </table>
+  )
+}
