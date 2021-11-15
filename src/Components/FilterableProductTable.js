cea11b1ed5fbf92bf82a471fdb8339871fc8a667
Brian Celine <brianceline@gmail.com>
2021-11-15 17:55:54 +0000

    complete initial work on shop

--- src/Components/FilterableProductTable.js
+++ src/Components/FilterableProductTable.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SearchBar from './SearchBar'
 import ProductTable from './ProductTable'
 
 export default function FilterableProductTable(props) {
   const { products } = props;
 
+  const [inStockOnly, setInStockOnly] = useState(false)
+
+  const handleInStockChange = (value) => {
+    setInStockOnly(value)
+  }
+
   return (
     <div>
-      <SearchBar />
-      <ProductTable products={products} />
+      <SearchBar
+        inStockOnly={inStockOnly}
+        onInStockChange={handleInStockChange}
+      />
+      <ProductTable
+        inStockOnly={inStockOnly}
+        products={products}
+      />
     </div>
   )
 }
--- src/Components/ProductTable.js
+++ src/Components/ProductTable.js
@@ -3,12 +3,16 @@ import ProductCategoryRow from './ProductCategoryRow'
 import ProductRow from './ProductRow'
 
 export default function ProductTable(props) {
-  const { products } = props;
+  const { products, inStockOnly } = props;
 
   const rows = [];
   let lastCategory = null;
 
   products.forEach((product) => {
+    if (inStockOnly && !product.stocked) {
+      return;
+    }
+
     if (product.category !== lastCategory) {
       rows.push(
         <ProductCategoryRow
